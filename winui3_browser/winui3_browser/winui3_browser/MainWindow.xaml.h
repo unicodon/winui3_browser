d7313a84e65c07c671497d52849e852fea5dd876
Yoshiaki Jitsukawa <yoshiaki.jitsukawa@sony.com>
2022-05-30 10:59:16 +0900

    Initial version

---
+++ winui3_browser/winui3_browser/winui3_browser/MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "MainWindow.g.h"
+
+namespace winrt::winui3_browser::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::winui3_browser::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
